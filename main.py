731e49bc27bc686bfd97640ef35e3cbd3fc9a9b4
syny-o <synek.o@seznam.cz>
2026-07-24 07:14:47 +0200

    created main_window_builder

--- main.py
+++ main.py
@@ -1,35 +1,24 @@
-import sys
-import pywinstyles
-
+import sys, pywinstyles
 from PyQt5.QtCore import Qt, QTimer, pyqtSignal, pyqtSlot
 from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import QApplication, QMainWindow, QSplitter, QVBoxLayout, QLabel, QFrame
-
+from PyQt5.QtWidgets import QApplication, QMainWindow
 from app_settings import AppSettings
 from application_lifecycle import ApplicationLifecycle
 from components.notification_widget import NotificationWidget
 from config.settings_controller import SettingsController
-from dashboard.dashboard import Dashboard
 from data_manager import project_manager
-from data_manager.data_manager import DataManager
 from data_manager.project_actions import ProjectActions
-from file_browser.tree_file_browser import FileSystemView
+from main_window_builder import MainWindowBuilder
 from text_editor.document_actions import DocumentActions
 from text_editor.editor_controller import EditorController
-from text_editor.outline_controller import OutlineController
-from text_editor.tab_manager import EditorTabManager
-from text_editor.tabs import Tabs
 from ui.main_ui import Ui_MainWindow
 from window_controller import WindowController
 from config.icon_manager import IconManager
-from components.widgets.widgets_pointing_hand import TreeWidgetPointingHand
 
 
 # pyinstaller -w --icon=R2Editor.ico --name=R2Editor main.py
 
-
 class MainWindow(QMainWindow, Ui_MainWindow):
-
     open_project = pyqtSignal(str)
     save_project = pyqtSignal(str)
     script_requirement_reference_changed = pyqtSignal(set, str)
@@ -52,32 +41,18 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.project_actions.connect_actions()
         self.document_actions = DocumentActions(self)
         
-
-        ################################################################################################################
         # APP SETTINGS CONFIGURATION
-        ################################################################################################################
         self.app_settings = AppSettings(self)
         self.editor_controller = EditorController(self)
         self.editor_controller.connect_actions(self.document_actions)
-
         
-        ################################################################################################################
         # GLOBAL TIMERS START
-        ################################################################################################################
-
         self.timer_project_autosave = QTimer()  # initialize timer - one global timer (even if it is not used - when value is Off)
         self.timer_project_autosave.timeout.connect(self.project_actions.autosave)
         self.settings_controller = SettingsController(self)
         self.settings_controller.apply()
 
-
-        ################################################################################################################
-        # GLOBAL TIMERS END
-        ################################################################################################################
-
-
         self.resize(1920, 1080)
-
         self.setWindowIcon(QIcon('R2Editor.ico'))
         self.setWindowTitle("Editor")
         self.frame_top.setVisible(False)
@@ -86,146 +61,42 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.frame_2.setVisible(False)
         self.window_controller.connect_window_actions()
 
-        self.VERSION = '2021-03-04'
-        ################################################################################################################
+        self.VERSION = '2026-07-24'
+        
         # POINTER TO ACTUAL TEXTEDIT, ACTUAL TABS
-        ################################################################################################################
         self._actual_text_edit = None
         self.actual_tabs = None 
+        self.window_builder = MainWindowBuilder(self, project_manager)
+        self.window_builder.build()
 
-        ################################################################################################################
-        # TREE FILE BROWSER CONFIGURATION
-        ################################################################################################################
-        uiLeftPanelSplitter = QSplitter()
-        uiLeftPanelSplitter.setOrientation(Qt.Vertical)
-        self.uiLayoutFileManager.addWidget(uiLeftPanelSplitter)
-        self.tree_file_browser = FileSystemView(self, project_manager)
-        uiLeftPanelSplitter.addWidget(self.tree_file_browser)
-
-        ################################################################################################################
-        # TREE OUTLINE CONFIGURATION
-        ################################################################################################################
-        
-        uiLayoutOutline = QVBoxLayout()
-        uiLayoutOutline.setContentsMargins(0, 0, 0, 0)
-        uiLayoutOutline.addWidget(QLabel("Outline"), alignment=Qt.AlignCenter)
-        self.uiTreeOutline = TreeWidgetPointingHand()
-        uiLayoutOutline.addWidget(self.uiTreeOutline)
-        self.uiTreeOutline.setHeaderHidden(True)
-        self.outline_controller = OutlineController(self, self.uiTreeOutline)
-        self.uiTreeOutline.itemClicked.connect(
-            self.outline_controller.click_item
-        )
-        self.timer_4_updating_outline = QTimer()
-        self.timer_4_updating_outline.timeout.connect(
-            self.outline_controller.update
-        )
-        self.timer_4_updating_outline.start(500)
-        uiFrameOutline = QFrame()
-        uiFrameOutline.setObjectName("objNameFrameOutline")
-        uiFrameOutline.setLayout(uiLayoutOutline)
-        uiLeftPanelSplitter.addWidget(uiFrameOutline)
-
-        ################################################################################################################
-        # DATA MANAGER CONFIGURATION
-        ################################################################################################################
-        self.data_manager = DataManager(self, project_manager)
-        self.script_requirement_reference_changed.connect(self.data_manager.script_requirement_reference_changed)
-
-        ################################################################################################################
-        # DASHBOARD CONFIGURATION
-        ################################################################################################################
-        self.dashboard = Dashboard(self, project_manager)
-
-        ################################################################################################################
-        # CONNECT PROJECT MANAGER WITH PROJECT LISTENERS
-        ################################################################################################################        
-        project_manager.set_listeners(self, self.dashboard, self.data_manager, self.tree_file_browser)
-
-        ################################################################################################################
-        # TABS CONFIGURATION
-        ################################################################################################################
-        self.left_tabs = Tabs(self, True, 'LEFT_TABS')
-        self.left_tabs.currentChanged.connect(
-            self.editor_controller.update_find_replace
-        )
-
-        self.right_tabs = Tabs(self, False, 'RIGHT_TABS')
-        self.right_tabs.currentChanged.connect(
-            self.editor_controller.update_find_replace
-        )
-
-        self.tab_manager = EditorTabManager(
-            self,
-            self.left_tabs,
-            self.right_tabs,
-        )
-        self.tab_manager.connect_signals()
-
-        self.tabs_splitter = QSplitter()
-        self.tabs_splitter.addWidget(self.left_tabs)
-        self.tabs_splitter.addWidget(self.right_tabs)
-        self.tabs_splitter.setStretchFactor(2, 1)
-
-        ################################################################################################################
-        # STACKEDWIDGET CONFIGURATION
-        ################################################################################################################
-        
-        self.stackedWidget.addWidget(self.tabs_splitter)
-        self.stackedWidget.addWidget(self.data_manager)
-        self.stackedWidget.addWidget(self.app_settings)
-        self.stackedWidget.addWidget(self.dashboard)
-        self.stackedWidget.setCurrentWidget(self.dashboard)
-
-        self.window_controller.connect_navigation()
-
-        ################################################################################################################
         # NOTIFICATION WIDGET CONFIGURATION
-        ################################################################################################################
         self.notification_widget = NotificationWidget(self)
-
         self.update_actual_information()
 
 
-
-    ################################################################################################################
-    # SETTINGS WAS UPDATED START
-    ################################################################################################################
 
     @pyqtSlot()
     def settings_was_updated(self):
         self.settings_controller.apply()
 
-
-
-
     @property
     def actual_text_edit(self):
         return self._actual_text_edit
 
-
     @actual_text_edit.setter
     def actual_text_edit(self, text_edit):
-        self._actual_text_edit = text_edit
-     
-
+        self._actual_text_edit = text_edit     
 
     def keyPressEvent(self, e) -> None:
         if e.key() == Qt.Key_Escape:
             self.editor_controller.close_find_replace()
         return super().keyPressEvent(e)
 
-
-
     def manage_right_menu(self, widget, button):
         self.window_controller.show_page(widget, button)
 
 
-
-
-########################################################################################################################
-# UPDATES:  START
-########################################################################################################################
+    # UPDATES:  START
     def receive_parameters_from_project_manager(self, parameters: dict):
         self.window_controller.update_project_title(parameters)
         
@@ -237,11 +108,6 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def show_notification(self, notification_text):
         self.window_controller.show_notification(notification_text)
 
-
-
-########################################################################################################################
-# UPDATES:  END
-########################################################################################################################
 
     # SAVE WINDOW SIZE, POSITION BEFORE CLOSE APP AND CHECK IF ALL SCRIPTS ARE SAVED
     def closeEvent(self, event):
---
+++ main_window_builder.py
@@ -0,0 +1,108 @@
+from PyQt5.QtCore import Qt, QTimer
+from PyQt5.QtWidgets import QFrame, QLabel, QSplitter, QVBoxLayout
+
+from components.widgets.widgets_pointing_hand import TreeWidgetPointingHand
+from dashboard.dashboard import Dashboard
+from data_manager.data_manager import DataManager
+from file_browser.tree_file_browser import FileSystemView
+from text_editor.outline_controller import OutlineController
+from text_editor.tab_manager import EditorTabManager
+from text_editor.tabs import Tabs
+
+
+class MainWindowBuilder:
+    def __init__(self, main_window, project_manager):
+        self.main_window = main_window
+        self.project_manager = project_manager
+
+    def build(self):
+        self._build_file_and_outline_panel()
+        self._build_application_pages()
+        self._build_editor_tabs()
+        self._build_page_stack()
+
+    def _build_file_and_outline_panel(self):
+        window = self.main_window
+        panel_splitter = QSplitter(Qt.Vertical)
+        window.uiLayoutFileManager.addWidget(panel_splitter)
+
+        window.tree_file_browser = FileSystemView(
+            window,
+            self.project_manager,
+        )
+        panel_splitter.addWidget(window.tree_file_browser)
+
+        outline_layout = QVBoxLayout()
+        outline_layout.setContentsMargins(0, 0, 0, 0)
+        outline_layout.addWidget(QLabel('Outline'), alignment=Qt.AlignCenter)
+
+        window.uiTreeOutline = TreeWidgetPointingHand()
+        window.uiTreeOutline.setHeaderHidden(True)
+        outline_layout.addWidget(window.uiTreeOutline)
+
+        window.outline_controller = OutlineController(
+            window,
+            window.uiTreeOutline,
+        )
+        window.uiTreeOutline.itemClicked.connect(
+            window.outline_controller.click_item
+        )
+        window.timer_4_updating_outline = QTimer()
+        window.timer_4_updating_outline.timeout.connect(
+            window.outline_controller.update
+        )
+        window.timer_4_updating_outline.start(500)
+
+        outline_frame = QFrame()
+        outline_frame.setObjectName('objNameFrameOutline')
+        outline_frame.setLayout(outline_layout)
+        panel_splitter.addWidget(outline_frame)
+
+    def _build_application_pages(self):
+        window = self.main_window
+        window.data_manager = DataManager(window, self.project_manager)
+        window.script_requirement_reference_changed.connect(
+            window.data_manager.script_requirement_reference_changed
+        )
+        window.dashboard = Dashboard(window, self.project_manager)
+        self.project_manager.set_listeners(
+            window,
+            window.dashboard,
+            window.data_manager,
+            window.tree_file_browser,
+        )
+
+    def _build_editor_tabs(self):
+        window = self.main_window
+        window.left_tabs = Tabs(window, True, 'LEFT_TABS')
+        window.left_tabs.currentChanged.connect(
+            window.editor_controller.update_find_replace
+        )
+        window.right_tabs = Tabs(window, False, 'RIGHT_TABS')
+        window.right_tabs.currentChanged.connect(
+            window.editor_controller.update_find_replace
+        )
+
+        window.tab_manager = EditorTabManager(
+            window,
+            window.left_tabs,
+            window.right_tabs,
+        )
+        window.tab_manager.connect_signals()
+
+        window.tabs_splitter = QSplitter()
+        window.tabs_splitter.addWidget(window.left_tabs)
+        window.tabs_splitter.addWidget(window.right_tabs)
+        window.tabs_splitter.setStretchFactor(2, 1)
+
+    def _build_page_stack(self):
+        window = self.main_window
+        for page in (
+            window.tabs_splitter,
+            window.data_manager,
+            window.app_settings,
+            window.dashboard,
+        ):
+            window.stackedWidget.addWidget(page)
+        window.stackedWidget.setCurrentWidget(window.dashboard)
+        window.window_controller.connect_navigation()
